f154707b71f0ccf80fc1eb36176ad8419b186290
Nihat <abdullazadenihat@gmail.com>
2023-04-03 23:22:49 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+
+            Console.WriteLine("Enter first number"); ;
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+           Console.WriteLine(num1 + num2);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        Console.WriteLine("Enter second number");
+    }
+
+    static int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
